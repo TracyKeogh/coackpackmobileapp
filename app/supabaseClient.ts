2e7a4b984e12899bb0b604e15d3a5f722b39f4eb
TracyKeogh <tracykeogh0@gmail.com>
2025-07-22 20:06:42 +0100

    Deleted supabaseClient.ts

--- app/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://bqxypskdebsesfwpqrfv.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxeHlwc2tkZWJzZXNmd3BxcmZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzNzE0MTksImV4cCI6MjA2NTk0NzQxOX0.xmg8dQFM9i9Dt33XPSOeHvAsA6s68LBdPCZurfz7X08';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
